26aebd0a43678132e118df08d08cca16989238df
donatellony <donatellony4736@gmail.com>
2024-05-04 17:10:38 +0200

    Added the logging and default API service configuration

--- YouAreShutUp.SteamBot/YouAreShutUp.SteamBot/Program.cs
+++ YouAreShutUp.SteamBot/YouAreShutUp.SteamBot/Program.cs
@@ -2,8 +2,22 @@ using YouAreShutUp.MessageBus;
 using YouAreShutUp.SteamBot.Steam.Configuration.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddAzureWebAppDiagnostics();
+});
 builder.AddSteam();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.AddYouAreShutUpAzureBus();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.Run();
